bed7814467dc70bf7523cdbe60c262f65e379b4b
skagos <prokopisstamelias@hotmail.com>
2020-12-10 05:51:55 +0200

    Second Commit

--- reduce.py
+++ reduce.py
@@ -1,5 +1,11 @@
+import sys
 def main():
-	print("Hello World");	
+	if sys.argv[1]=='-d':
+		dataset=sys.argv[2]
+		output=sys.argv[4]
+	if sys.argv[1]=='-o':
+		dataset=sys.argv[4]
+		output=sys.argv[2]
 
 if __name__ == "__main__":
 	main()
